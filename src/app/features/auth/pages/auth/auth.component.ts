5f361853dc539f89b66d9292a294aeef70aac1e8
kaisyQ <rutyo8@mail.ru>
2024-06-03 13:11:54 +1000

    remove useless imports

--- src/app/features/auth/pages/auth/auth.component.ts
+++ src/app/features/auth/pages/auth/auth.component.ts
@@ -1,5 +1,3 @@
-import { LoginComponent } from './../../components/login/login.component';
-import { RegisterComponent } from './../../components/register/register.component';
 import { Component } from "@angular/core";
 import { MatTabsModule } from '@angular/material/tabs';
 import { RouterModule } from '@angular/router';
@@ -9,8 +7,6 @@ import { RouterModule } from '@angular/router';
     templateUrl: './auth.component.html',
     standalone: true,
     imports: [
-        LoginComponent,
-        RegisterComponent,
         MatTabsModule,
         RouterModule
     ]
